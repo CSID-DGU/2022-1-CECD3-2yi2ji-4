84107bde80336918f48c8e739a62a0053f71bc7f
wonny7447 <wonny7447@naver.com>
2022-12-11 14:18:35 +0900

    feat: modify project struct & add barcode function

--- mainSite/kiosk/urls.py
+++ mainSite/kiosk/urls.py
@@ -6,4 +6,11 @@ app_name = "kiosk" # 앱 이름 설정
 urlpatterns = [
     path('', views.index, name='index'),
     path('upload', views.upload, name='upload'),
+    path('testIndex', views.objectIndex, name='objectIndex'),
+    path('kakaoApi/', views.kakaoApi, name="kakaoApi"),
+    path('sttFileApi/', views.sttFileApi, name="sttFileApi"),
+    path('sttMicApi/', views.sttMicApi, name="sttMicApi"),
+    path('ttsApi/', views.ttsApi, name="ttsApi"),
+    path('roi/', views.roi, name="roi"),
+    path('roiResult/', views.roiResult, name="roiResult"),
 ]
